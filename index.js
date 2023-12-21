f4299fc8b09c43d7600bc1e5a248e418f21927bc
hongbowu <wuhongbo87@gmail.com>
2023-12-20 22:10:29 -0500

    fix the bug and update info

---
+++ index.js
@@ -0,0 +1,46 @@
+const Shapes = require("./lib/shapes");
+const Text = require("./lib/text");
+const svgGenerator = require("./lib/svgGenerator")
+
+const inquirer = require("inquirer")
+const fs = require("fs")
+
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, "utf-8",(error)=> error ? console.error(error): console.log("success!"))
+}
+
+function init() {
+    inquirer
+    .prompt([
+        {
+            type:"input",
+            message:"You can enter up to 3 characters",
+            name:"textInput"
+        },
+        {
+            type:"input",
+            message:"You can enter a color keyword or a hexadecimal number for the text color",
+            name:"textColor"
+        },
+        {
+            type:"list",
+            message:"You can chose a shape",
+            name:"shapesChoice",
+            choices:["circle", "square", "rectangle"]
+        },
+        {
+            type:"input",
+            message:"You can enter a color keyword or a hexadecimal number for the shape's color",
+            name:"shapeColor"
+        }
+    ])
+    .then((response)=>{
+        const {textInput, textColor, shapesChoice, shapeColor} = response
+        const shapes = new Shapes()
+        const text = new Text()
+        const shape = shapes[shapesChoice](`"${shapeColor}"`)
+        const insert = text.input(`${textInput}`,`"${textColor}"`)
+        writeToFile("./examples/logo.svg", svgGenerator(shape,insert))
+    })
+}
+init()
